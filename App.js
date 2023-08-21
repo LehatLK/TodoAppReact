a0c3070286b046eea1bf5b9bddf63dbde8319230
Lehat Khalid <lehatk@hotmail.se>
2023-08-21 15:18:51 +0200

    test merge conf

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import Addbutton from './COMPONENT/addButton';
+import DeleteButton from './COMPONENT/deleteButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>heloo</Text>
+      <Addbutton></Addbutton>
+      <DeleteButton></DeleteButton>
       <StatusBar style="auto" />
     </View>
   );
