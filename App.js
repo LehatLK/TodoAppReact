8dc1552d49bf1b01d7c8a5d3c4b742c13990f90f
muza97 <mustafa.zaitoun97@gmail.com>
2023-08-21 15:58:57 +0200

    apperingNotes

--- App.js
+++ App.js
@@ -1,26 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-//import Addbutton from './COMPONENT/addButton';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import DeleteButton from './COMPONENT/deleteButton';
 import NotesTextField from './COMPONENT/notesTextField';
 
 export default function App() {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (note) => {
+    setNotes([...notes, note]);
+  };
+
+  const deleteNote = (index) => {
+    const updatedNotes = notes.filter((_, i) => i !== index);
+    setNotes(updatedNotes);
+  };
+
   return (
     <View style={styles.container}>
-
-      <DeleteButton></DeleteButton>
-      <NotesTextField onAdd={(note) => console.log('Added', note)} />
+      <View style={styles.content}>
+        <Text>Todo List App</Text>
+        <ScrollView style={styles.notesList}>
+          {notes.map((note, index) => (
+            <View key={index} style={styles.noteItem}>
+              <Text>{note}</Text>
+              <DeleteButton onDelete={() => deleteNote(index)} />
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+      <NotesTextField onAdd={addNote} />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#89CFF0',
+  },
+  content: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
+  },
+  notesList: {
+    maxHeight: 200, // Set a maximum height for the notes list
+    width: '100%',
+  },
+  noteItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
   },
 });
